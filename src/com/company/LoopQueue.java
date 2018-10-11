c03f6b7970ef58b2eca3911233424602913d63d3
zsy <shuangyuzhu0069@gmail.com>
2018-10-11 11:34:10 +0800

    comparision of loopqueue and arrayqueue about time complexity

--- src/com/company/LoopQueue.java
+++ src/com/company/LoopQueue.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class LoopQueue<E> implements Queue {
+public class LoopQueue<E> implements Queue<E> {
     private E[] data;
     private int front,tail;
     private int size;
@@ -30,20 +30,81 @@ public class LoopQueue<E> implements Queue {
     @Override
     public boolean isEmpty() {
         return front == tail;
-    }
-
-    @Override
-    public void enqueue(Object o) {
 
     }
 
     @Override
-    public Object dequeue() {
-        return null;
+    public void enqueue(E e) {
+
+
+        if((tail + 1) % data.length == front)
+            resize(getCapacity() * 2);
+
+        data[tail] = e;
+        tail = (tail + 1) % data.length;
+        size ++;
+
     }
 
     @Override
-    public Object getFront() {
-        return null;
+    public E dequeue() {
+
+        if(isEmpty())
+            throw new IllegalArgumentException("Cannot dequeue from an empty queue.");
+
+        E ret = data[front];
+        data[front] = null;
+        front = (front + 1) % data.length;
+        size --;
+        if(size == getCapacity() / 4 && getCapacity() / 2 != 0)
+            resize(getCapacity() / 2);
+        return ret;
+    }
+
+    @Override
+    public E getFront() {
+        if(isEmpty())
+            throw new IllegalArgumentException("Queue is empty.");
+        return data[front];
+    }
+
+    // 将数组空间的容量变成newCapacity大小
+    private void resize(int newCapacity){
+        E[] newData = (E[])new Object[newCapacity + 1];
+        for(int i = 0 ; i < size ; i ++)
+            newData[i] = data[(i + front) % data.length];
+
+        data = newData;
+        front = 0;
+        tail = size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Queue: size = %d , capacity = %d\n", size, getCapacity()));
+        res.append("front [");
+        for(int i = front ; i != tail ; i = (i + 1) % data.length){
+            res.append(data[i]);
+            if((i + 1) % data.length != tail)
+                res.append(", ");
+        }
+        res.append("] tail");
+        return res.toString();
+    }
+
+
+    public static void main(String[] args){
+
+        LoopQueue<Integer> queue = new LoopQueue<>();
+        for(int i = 0 ; i < 10 ; i ++){
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if(i % 3 == 2){
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
     }
 }
