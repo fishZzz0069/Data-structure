6de52f3bf4c0ff64d0b3959c728b8782515c8197
zsy <shuangyuzhu0069@gmail.com>
2018-10-11 11:26:14 +0800

    loopqueue implementation

--- src/com/company/ArrayQueue.java
+++ src/com/company/ArrayQueue.java
@@ -58,12 +58,14 @@ public class ArrayQueue<E> implements Queue<E>{
         return res.toString();
     }
 
-    public static void main(String[] args) {
 
-        ArrayQueue<Integer> queue = new ArrayQueue<>();
+    public static void main(String[] args){
+
+        LoopQueue<Integer> queue = new LoopQueue<>();
         for(int i = 0 ; i < 10 ; i ++){
             queue.enqueue(i);
             System.out.println(queue);
+
             if(i % 3 == 2){
                 queue.dequeue();
                 System.out.println(queue);
