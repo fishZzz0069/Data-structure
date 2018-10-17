795ed45e864fb96b00840377519a5d9ed3445597
zsy <shuangyuzhu0069@gmail.com>
2018-10-17 11:42:21 +0800

    Binary search tree

---
+++ src/com/company/BST/BST.java
@@ -0,0 +1,37 @@
+package com.company.BST;
+
+import com.company.linkedlist.LinkedListQueue;
+
+public class BST<E extends Comparable<E>> {
+
+    private class Node{
+
+        public E e;
+        public BST.Node left , right;
+
+        public Node(E e){
+            this.e = e;
+            left = null;
+            right = null;
+        }
+
+    }
+
+    private Node root;
+    private int size;
+
+    public BST(){
+        root = null;
+        size = 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+
+}
